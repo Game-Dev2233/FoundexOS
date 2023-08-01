d3791c1ab420cf769a707bedc7899e73151eb49c
Game-Dev2233 <owen.winchell@gmail.com>
2023-08-01 13:22:34 -0400

    updated printf.c then downgraded it

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -3,7 +3,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-
 static bool print(const char* data, size_t length) {
 	const unsigned char* bytes = (const unsigned char*) data;
 	for (size_t i = 0; i < length; i++)
