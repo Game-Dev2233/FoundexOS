56dcba8cbc3875bb26c1a56c47ca837fc9b62e53
Game Dev <owen.winchell@gmail.com>
2023-08-22 08:49:43 -0400

    Updated it to work as a Simple Terminal

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 #include <kernel/tty.h>
 
-void kernel_main(void) {
-	terminal_initialize();
-	printf("Hello, kernel World!");
+// Function to execute a command
+void executeCommand(const char *command) {
+    // Implement code to execute the specified command here.
+    // For this example, we'll execute "hello" and "bye" commands.
+
+    if (strcmp(command, "hello") == 0) {
+        printf("Hello, World!\n");
+    } else if (strcmp(command, "bye") == 0) {
+        printf("Goodbye!\n");
+    } else {
+        printf("Unknown command: %s\n", command);
+    }
+}
+
+// Function to read user input from GRUB
+void readInput(char *buffer, int maxLength) {
+    asm volatile (
+        // Read user input from GRUB's command line.
+        "mov $0x16, %%eax\n"
+        "mov $0, %%ebx\n"            // File descriptor (stdin)
+        "mov %0, %%ecx\n"            // Address of buffer
+        "mov %1, %%edx\n"            // Maximum input length
+        "int $0x80\n"
+
+        // Null-terminate the input.
+        "movb $0, (%0, %%eax, 1)\n"
+        
+        : : "m"(buffer), "i"(maxLength)
+        : "eax", "ebx", "ecx", "edx"
+    );
+}
+
+int main() {
+    char inputBuffer[256];
+
+    while (1) {
+        // Display a prompt.
+        printf("MyOS> ");
+
+        // Read user input from GRUB
+        readInput(inputBuffer, sizeof(inputBuffer));
+
+        // Execute the user's command.
+        executeCommand(inputBuffer);
+    }
+
+    return 0;
 }
